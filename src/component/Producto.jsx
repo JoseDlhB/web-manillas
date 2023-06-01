085f4e2ddc4644e659c2ea9ec9f664f50871561a
Jose DlhB <josehoz.ing@gmail.com>
2023-05-31 23:20:30 -0500

    Componente Producto, config variables

--- src/component/Producto.jsx
+++ src/component/Producto.jsx
@@ -1,15 +1,39 @@
 import React, {useEffect, useState} from 'react'
-import { db } from '../firebase'
-import {collection, doc, query, getDoc} from 'firebase/firestore';
+import {db} from '../firebase'
+import {collection, doc, onSnapshot, query} from 'firebase/firestore'
 
-const Producto = () => {
+const [dijes, setDijes] = useState([]);
+const [materialSeleccionado, setMaterialSeleccionado] = useState('');
+const [dijeSeleccionado, setDijeSeleccionado] = useState('');
+const [tipoSeleccionado, setTipoSeleccionado] = useState('');
+const [pagoSeleccionado, setPagoSeleccionado] = useState('');
+const [opcionesSeleccionadas, setOpcionesSeleccionadas] = useState('');
 
-  return (
-    <div>
-        <h2>Manillas disponibles:</h2>
-    </div>
-  )
-}
+    useEffect(() => {
+        const obtenerDijes = async() => {
+            try{ await onSnapshot(
+                collection(db, 'manillas'), (query) =>{
+                    setDijes(query.docs.map((doc) => ({...doc.data(), id:doc.id})))
+                })
+            }catch(error){
+            console.log(e)
+            }
+        }
+        obtenerDijes();
+     }, [])
+
+
+    useEffect(() => {
+        const opciones = `${materialSeleccionado} ${dijeSeleccionado} ${tipoSeleccionado} ${pagoSeleccionado}`;
+        setOpcionesSeleccionadas(opciones);
+    }, [materialSeleccionado, dijeSeleccionado, tipoSeleccionado, pagoSeleccionado]);
+
+    return (
+        <div>
+            <h2>Manillas disponibles:</h2>
+        </div>
+    )
+    }
 
 export default Producto
 
